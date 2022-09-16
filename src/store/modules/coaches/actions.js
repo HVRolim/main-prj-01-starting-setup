c35e884f78acc78bb85a87748c9c61fe04c32848
Haroldo Rolim <haroldo.rolim@capim.com.br>
2022-09-16 14:25:57 -0300

    sending-put-http-request

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,27 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
+    };
+
+    const response = await fetch(`https://vue-http-demo-edc63-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
+
+    // const responseData = await response.json();
+    if (!response.ok) {
+      // error...
     }
-    context.commit('registerCoach', coachData)
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    })
   }
 };
