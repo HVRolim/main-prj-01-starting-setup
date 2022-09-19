efa93b00031eced097681a71a4381cb97108b2d2
Haroldo Rolim <haroldo.rolim@capim.com.br>
2022-09-19 15:14:30 -0300

    caching-http-response-data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -24,7 +24,11 @@ export default {
       id: userId
     })
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(`https://vue-http-demo-edc63-default-rtdb.firebaseio.com/coaches.json`);
     const responseData = await response.json();
 
@@ -45,7 +49,8 @@ export default {
       };
       coaches.push(coach);
     }
-    context.commit('setCoaches', coaches)
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
 
   }
 };
